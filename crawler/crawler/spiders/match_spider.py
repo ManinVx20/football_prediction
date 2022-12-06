b6642d3bb243f2f969e9c3bccc6271ffe12b19b8
“ManInVx20” <vinhlb.2k@gmail.com>
2022-12-06 21:01:56 +0700

    Change website to crawl data

--- crawler/crawler/spiders/match_spider.py
+++ crawler/crawler/spiders/match_spider.py
@@ -11,25 +11,26 @@ class MatchSpider(scrapy.Spider):
     def start_requests(self):
         tag = getattr(self, 'tag', None)
         if tag is not None:
-            url = "https://web.archive.org/web/20221115040351/https://en.wikipedia.org/wiki/2022_FIFA_World_Cup"
+            url = "https://web.archive.org/web/20220809144459/https://www.thesoccerworldcups.com/world_cups/2022_results.php"
             yield scrapy.Request(url, self.parse)
         else:
             for year in self.years:
-                url = f"https://en.wikipedia.org/wiki/{year}_FIFA_World_Cup"
+                url = f"https://www.thesoccerworldcups.com/world_cups/{year}_results.php"
                 yield scrapy.Request(url, self.parse)
 
     def parse(self, response):
-        matches = response.xpath("//div[@class='footballbox']")
+        year = response.url.split("/")[-1][0:4]
+        matches = response.xpath("//div[contains(@class, 'game') and contains(@class, 'clearfix')]")
         for match in matches:
-            home = match.xpath(".//th[@class='fhome']/span//text()").extract()
+            home = match.xpath(".//div[@class='left margen-b2 clearfix']/div[contains(@class, 'left ')]/text()").extract()
             if len(home) > 1:
                 home = home[0]
 
-            score = match.xpath(".//th[@class='fscore']//text()").extract()
+            score = match.xpath(".//div[@class='left a-center margen-b3 clearfix']/div[@class='left wpx-60']/a/text()").extract()
             if len(score) > 1:
                 score = "".join(score)
             
-            away = match.xpath(".//th[@class='faway']/span//text()").extract()
+            away = match.xpath(".//div[@class='left a-left margen-b2 clearfix']/div[contains(@class, 'left ')]/text()").extract()
             if len(away) > 1:
                 away = away[1]
 
@@ -37,4 +38,5 @@ class MatchSpider(scrapy.Spider):
                 "home": home,
                 "score": score,
                 "away": away,
+                "year": year,
             }
